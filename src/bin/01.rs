b7d6dec5d971891644aa4d52d3cc0586a638bcd2
Johnny Urosevic <themagicalcake@gmail.com>
2023-12-17 12:31:04 -0800

    Day 1 Part 2 and Part 1 refactor

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,24 +1,66 @@
+use regex::Regex;
 advent_of_code::solution!(1);
 
+fn get_first_numeric_digit(l: &str) -> u32 {
+    l.chars()
+        .find(|c| c.is_numeric())
+        .expect("Input must have digit")
+        .to_digit(10)
+        .expect("Filter should only include digits")
+}
+
+fn reverse_string(l: &str) -> String {
+    l.chars().rev().collect::<String>()
+}
+
+fn get_last_numeric_digit(l: &str) -> u32 {
+    get_first_numeric_digit(&reverse_string(l))
+}
+
+fn trebuchet(input: &str, first_digit_strategy: fn(&str) -> u32, last_digit_strategy: fn(&str) -> u32) -> Option<u32>
+{
+    let answer = input
+        .lines()
+        .map(|l| {
+            first_digit_strategy(l) * 10 + last_digit_strategy(l)
+        })
+        .sum();
+
+    Some(answer)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-        let digits = input
-            .lines()
-            .map(|l| {
-                l.chars()
-                    .filter(|c| c.is_numeric())
-                    .map(|c| c.to_digit(10).expect("Filter should be numeric only"))
-                    .collect()
-            });
+    trebuchet(input, get_first_numeric_digit, get_last_numeric_digit)
+}
 
-        let answer = digits
-            .map(|arr: Vec<u32>| 10 * arr.first().unwrap() + arr.last().unwrap())
-            .sum();
+const WORD_REGEX: &str = r"one|two|three|four|five|six|seven|eight|nine";
 
-        Some(answer)
+fn do_regex_search(l: &str, re: Regex) -> u32 {
+    match re.find(l).expect("Must be at least one digit in input").as_str() {
+        "one" | "eno" => 1,
+        "two" | "owt" => 2,
+        "three" | "eerht" => 3,
+        "four" | "ruof" => 4,
+        "five" | "evif" => 5,
+        "six" | "xis" => 6,
+        "seven" | "neves" => 7,
+        "eight" | "thgie" => 8,
+        "nine" | "enin" => 9,
+        digit if digit.chars().next().unwrap().is_numeric() => digit.chars().next().unwrap().to_digit(10).unwrap(),
+        _ => unreachable!(),
+    }
+}
+
+fn get_first_digit(l: &str) -> u32 {
+    do_regex_search(l, Regex::new(&format!("{WORD_REGEX}|[0-9]")).unwrap())
+}
+
+fn get_last_digit(l: &str) -> u32 {
+    do_regex_search(&reverse_string(l), Regex::new(&format!("{}|[0-9]", reverse_string(WORD_REGEX))).unwrap())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    trebuchet(input, get_first_digit, get_last_digit)
 }
 
 #[cfg(test)]
@@ -33,7 +75,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(281));
     }
 }
